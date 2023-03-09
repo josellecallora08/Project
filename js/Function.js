0ab336c7c62aeca37451602b608e33e96508e2f0
Soj <josellecallora08@gmail.com>
2023-03-09 19:20:47 +0800

    Committing all files

---
+++ js/Function.js
@@ -0,0 +1,6 @@
+const sidebar = document.getElementById('sidebar');
+const iconMenu = document.getElementById('icon-sidebar');
+
+function sidebarToggle(){
+    document.querySelector('body').classList.toggle('active');
+}
